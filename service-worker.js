f08c07bf493dafab270eb92dcbe7a60ada7ea97d
barranger <barranger@gmail.com>
2019-02-25 23:19:32 +0000

    Automated Release 7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/every-day/precache-manifest.5ab9948014dd2e47a5b6ab341e318849.js"
+  "/every-day/precache-manifest.73f0c75636176bb22db43d10e66f4d67.js"
 );
 
 workbox.clientsClaim();
