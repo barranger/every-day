ebe33117df0f5b11acfe4ebdd7e99342a455577d
barranger <barranger@gmail.com>
2019-03-01 04:31:27 +0000

    Automated Release 11

---
+++ precache-manifest.e303a7a2a2d2d93b1b5f7e6305964857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2d67dadbce0718cde84",
+    "url": "/every-day/static/js/runtime~main.c2d67dad.js"
+  },
+  {
+    "revision": "ce7bd974197721af874b",
+    "url": "/every-day/static/js/main.ce7bd974.chunk.js"
+  },
+  {
+    "revision": "b0b0f073511fea7b9c22",
+    "url": "/every-day/static/js/2.b0b0f073.chunk.js"
+  },
+  {
+    "revision": "ce7bd974197721af874b",
+    "url": "/every-day/static/css/main.7b1b653e.chunk.css"
+  },
+  {
+    "revision": "3b486eb2b14466e1f4768a6dd2a5684c",
+    "url": "/every-day/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/every-day/precache-manifest.39dac6ce112f2c1c8f108ac961530187.js"
+  "/every-day/precache-manifest.e303a7a2a2d2d93b1b5f7e6305964857.js"
 );
 
 workbox.clientsClaim();
